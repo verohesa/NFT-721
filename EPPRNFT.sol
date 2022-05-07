ddc9a888f0d4315164499583d52e25b0fa77c909
EPPR LXPS <67383945+eppr-lxps@users.noreply.github.com>
2022-05-07 10:52:35 -0500

    Update EPPRNFT.sol

--- EPPRNFT.sol
+++ EPPRNFT.sol
@@ -1,6 +1,6 @@
 // EPPR - NFT 721 para OpenSea.
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
